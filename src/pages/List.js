d904db30b5f5977b99bfc3fa41c231b103e6f3bc
bae080311 <s24024@gsm.hs.kr>
2024-08-18 05:07:45 +0900

    feat: show memo

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,14 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Title from "../components/Title";
-import Button from "../components/button";
+import Button from "../style/button";
+import styled from "styled-components";
+import axios from "axios";
 
-function List() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column; /* 수정: 수직 정렬을 위한 flex-direction 추가 */
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 70%;
+  background-color: purple;
+  color: white;
+`;
+
+function ShowMemo({ memo }) {
+  return (
+    <Container
+      style={{
+        flexDirection: "row",
+      }}
+    >
+      <p>{memo.content}</p>
+    </Container>
+  );
+}
+
+export default function List() {
+  const [memos, setMemos] = useState([]);
   const navigate = useNavigate();
+
+  const getMemo = () => {
+    axios
+      .get("http://localhost:5000/memos")
+      .then((res) => {
+        setMemos(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMemo();
+  }, []);
 
   return (
     <div className="App">
       <Title />
+      <Container
+        style={{
+          marginLeft: "25%",
+        }}
+      >
+        {memos.map((memo) => (
+          <ShowMemo key={memo.id} memo={memo} />
+        ))}
+      </Container>
       <Button
         onClick={() => {
           navigate("/create");
@@ -16,7 +66,7 @@ function List() {
         style={{
           position: "relative",
           left: "47%",
-          top: "500px",
+          top: "20px",
         }}
       >
         메모 작성
@@ -24,5 +74,3 @@ function List() {
     </div>
   );
 }
-
-export default List;
