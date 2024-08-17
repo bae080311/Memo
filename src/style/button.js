5beeba8db7eb0636e538f3e69a0e257b4e951b19
bae080311 <s24024@gsm.hs.kr>
2024-08-18 04:21:02 +0900

    fix: 파일위치

--- src/style/button.js
+++ src/style/button.js
@@ -5,8 +5,9 @@ const Button = styled.button`
   color: white;
   border: none;
   padding: 10px 20px;
-  border-radius: 5px;
+  border-radius: 10px;
   cursor: pointer;
+  font-weight: bold;
 `;
 
 export default Button;
