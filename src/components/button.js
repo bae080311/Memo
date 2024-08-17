f766906a5ccfe5335099a520c6b938ae7a3781fb
bae080311 <s24024@gsm.hs.kr>
2024-08-17 23:19:37 +0900

    feat: Button

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default Button;
